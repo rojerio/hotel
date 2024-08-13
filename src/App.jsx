f7f058a89d1a79ec7eaa71e9b7b84487f27335c3
rojerio <renan_jussiani@estudante.sesisenai.org.br>
2024-08-13 12:13:20 -0300

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import './App.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Aqui você pode adicionar a lógica de autenticação, se necessário
+    navigate('/Home'); // Redireciona para a página inicial ou para onde desejar
+  };
+
+  const handleRegister = () => {
+    navigate('/Register'); // Redireciona para a página de cadastro
+  };
 
   return (
     <>
-    <div className="container">
-      <div className="aling">
-        <h1>LOGO</h1>
-        <h4>Gmail</h4>
-        <input type="email" placeholder='Gmail'/>
-        <h4>Senha</h4>
-        <input type="password" placeholder='Senha'/>
-
+      <div className="container">
+        <div className="align">
+          <h1>LOGO</h1>
+          <h4>Gmail</h4>
+          <input type="email" placeholder='Gmail' />
+          <h4>Senha</h4>
+          <input type="password" placeholder='Senha' />
+        </div>
+        <div className="link">
+          <button onClick={handleLogin}>ENTRAR</button>
+          <p>Não é registrado ainda? <a className='a' onClick={handleRegister}>Cadastrar-se</a></p>
+        </div>
       </div>
-      <div className="link">
-        <button>ENTRAR</button>
-
-        <p>Não é registrado ainda ? <a className='a'>Cadastrar-se</a></p>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <>
+        <p>oi</p>
+        </>
+    )
+}
+
+export default Home;
